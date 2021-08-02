ac2914fec2f313bdc1177915bea87fd3022047d4
Nikola <88djony@gmail.com>
2021-08-02 11:09:37 -0700

    working on landing page

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,9 +3,8 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-
-        </div>
+    <div>
+    </div>
 
     )
 }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -4,8 +4,7 @@ import React from 'react';
 const Work = () => {
     return (
         <div>
-
-        </div>
+    </div>
 
     )
 }
