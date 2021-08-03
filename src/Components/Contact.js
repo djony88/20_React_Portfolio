d2540d9749f1c1ea2946812b57be67c406bd01ac
Nikola <88djony@gmail.com>
2021-08-03 11:31:37 -0700

    added variables to render in about, contact and portfolio components

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,11 +3,21 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
+        <div className="row">
+            <div id= "Contact-me" className="Contact-me">
+                <h4>
+                    <span className="badge bg-info text-dark">Contact me</span>
+                </h4>
+                <ul className="nav nav-pills">
+                    <a className="nav-link active" aria-current="page" href="tel:619-808-8102">619.808.8102</a>
+                    
+                    <a className="nav-link active" aria-current="page" href="mailto: 88djony@gmail.com">Send Email</a>
+                    </ul>
 
+                </div>
         </div>
 
     )
-}
+};
 
 export default Contact;
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,9 +3,125 @@ import React from 'react';
 
 const Work = () => {
     return (
-        <div>
-    </div>
+        <div className="row">
+            ::before
+            <div className="columns collapsed">
+                <h1>PROJECTS</h1>
+                <div id="portfolio-wrapper" className="bgrid-quaters s-bgrid-thirds cf">
+                    <div className="item-wrap">
+                        <a href="https://djony88.github.io/05_Work_Day_Scheduler/" target="_blank" title="Work Day Scheduler" rel="noreferrer">
+                            <img alt="Work Day Scheduler" src="img/05-Work-Day-Scheduler.png"></img>
+                            <div className="overlay">
+                                <div className="portfolio-item-meta">
+                                    <h5>Work Day Scheduler</h5>
+                                    <p>HTML, CSS, JS</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
 
+                <div id="portfolio-wrapper" className="bgrid-quaters s-bgrid-thirds cf">
+                    <div className="item-wrap">
+                        <a href="https://djony88.github.io/06_Weather_App/" target="_blank" title="Weather App" rel="noreferrer">
+                            <img alt="Weather App" src="img/Weather_App.png"></img>
+                            <div className="overlay">
+                                <div className="portfolio-item-meta">
+                                    <h5>Weather App</h5>
+                                    <p>HTML, CSS, JS, API, Ajax</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div id="portfolio-wrapper" className="bgrid-quaters s-bgrid-thirds cf">
+                    <div className="item-wrap">
+                        <a href="https://jg-77.github.io/Movie-Buffs-Group-Project/?" target="_blank" title="Movie Buffs" rel="noreferrer">
+                            <img alt="Movie Buffs" src="img/Movie_Buffs.png"></img>
+                            <div className="overlay">
+                                <div className="portfolio-item-meta">
+                                    <h5>Movie Buffs</h5>
+                                    <p>HTML, CSS, JS, API, Ajax, JQuery, SweetAlerts2, Pure CSS, Media Query</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div id="portfolio-wrapper" className="bgrid-quaters s-bgrid-thirds cf">
+                    <div className="item-wrap">
+                        <a href="https://github.com/djony88/10_README_Generator" target="_blank" title="README Generator" rel="noreferrer">
+                            <img alt="README Generator" src="img/README_generator.gif"></img>
+                            <div className="overlay">
+                                <div className="portfolio-item-meta">
+                                    <h5>README Generator</h5>
+                                    <p>Node.js</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div id="portfolio-wrapper" className="bgrid-quaters s-bgrid-thirds cf">
+                    <div className="item-wrap">
+                        <a href="https://damp-river-55680.herokuapp.com/" target="_blank" title="Note Taker App" rel="noreferrer">
+                            <img alt="Note Taker App" src="img/Notes_App.png"></img>
+                            <div className="overlay">
+                                <div className="portfolio-item-meta">
+                                    <h5>Note Taker App</h5>
+                                    <p>HTML, CSS, JS, Node.js, Express, Heroku </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div id="portfolio-wrapper" className="bgrid-quaters s-bgrid-thirds cf">
+                    <div className="item-wrap">
+                        <a href="https://github.com/djony88/12_Employee_Tracker" target="_blank" title="Employee Tracker" rel="noreferrer">
+                            <img alt="Employee Tracker" src="img/Employee_ Tracker.gif"></img>
+                            <div className="overlay">
+                                <div className="portfolio-item-meta">
+                                    <h5>Employee Tracker</h5>
+                                    <p>Node.js, MySQL</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div id="portfolio-wrapper" className="bgrid-quaters s-bgrid-thirds cf">
+                    <div className="item-wrap">
+                        <a href="https://gentle-sands-99944.herokuapp.com/" target="_blank" title="Tech Blog App" rel="noreferrer">
+                            <img alt="Tech Blog App" src="img/Tech_Blog_App.PNG"></img>
+                            <div className="overlay">
+                                <div className="portfolio-item-meta">
+                                    <h5>Tech Blog App</h5>
+                                    <p>Handlebars, Node.js, MySQL, CSS, JS</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div id="portfolio-wrapper" className="bgrid-quaters s-bgrid-thirds cf">
+                    <div className="item-wrap">
+                        <a href="https://hotel-coding.herokuapp.com/" target="_blank" title="Hotel Coding" rel="noreferrer">
+                            <img alt="Hotel Coding" src="img/Hotel_Coding.PNG"></img>
+                            <div className="overlay">
+                                <div className="portfolio-item-meta">
+                                    <h5>Hotel Coding</h5>
+                                    <p>Handlebars, Node.js, MySQL, CSS, JS, Heroku, npm libraries: dotenv, express, express-handlebars, express-session, bcrypt, bad-words</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
     )
 }
 
